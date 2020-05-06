7bf2be99be44d1fc0463d5c49f772ea857e45412
David Tang <43974221+itangdav@users.noreply.github.com>
2020-05-06 10:15:31 -0400

    Add files via upload

---
+++ bin/config.py
@@ -0,0 +1,4 @@
+#Configuration file
+
+#This is the height of the individual in meters
+height = 1.8;
---
+++ bin/main.py
@@ -0,0 +1,9 @@
+# This python file runs the main simulation and provides the output data to the Scatterplot file
+
+# Import from config
+
+from bin.config import *
+
+print(height)
+
+
