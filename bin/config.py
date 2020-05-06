1852f79568ed1bf72f8dfb665a3ac26106dc0f5d
David Tang <43974221+itangdav@users.noreply.github.com>
2020-05-06 19:54:43 -0400

    Finished model with no clothing

--- bin/config.py
+++ bin/config.py
@@ -1,4 +1,35 @@
 #Configuration file
 
-#This is the height of the individual in meters
-height = 1.8;
+# Parameters for Simulation
+
+# The step of time between each data point in seconds
+stepSize = 60
+# The total number of steps taken until end of simulation
+totalStep = 100
+
+
+# Parameters for Human Model
+
+# Body temperature of person in degrees Kelvin (add 273.15 to Celsius temperature)
+bodyTemp = (37) + 273.15
+# Temperature of outside surroundings in degrees Kelvin
+outerTemp = (29) + 273.15
+# Height of person in meters
+height = 1.72
+# Radius of person in direction parallel to floor in meters
+radius = 0.15
+# Weight of person in kg
+weight = 70
+# Number of clothing worn
+numberOfClothing = 2
+# An array for the distance of the outer layer of each piece of clothing from center axis in meters
+# This must be sorted in increasing order and each distance must be greater than the radius of the person
+distanceOfClothing = [0.16, 0.17]
+# An array for thermal conductivity coefficients k given in W m^-1 K^-1 for each layer of clothing
+# This must be in the same order as the previous array and the same size
+k = [0.039, 0.14]
+# Specific heat capacity of outer layer of clothing in J kg^-1 K^-1 (if numberOfClothing = 0, put 0)
+cOuter = 1500
+# Density of outer layer of clothing in kg m^-3
+density = 860
+
--- bin/main.py
+++ bin/main.py
@@ -3,7 +3,50 @@
 # Import from config
 
 from bin.config import *
+import math
 
-print(height)
+STEFANBOLTZMANNCONSTANT = 5.670373 * math.pow(10, -8)
+WATERHEATCAPACITY = 4186
+
+# Store the data in arrays
+bodyTempData = [bodyTemp]
+time = [0]
+
+# First, we seek to calculate the heat lost from body without clothing in case numberOfClothing =0
+
+# All heat lost in our scenario is due to radiation
+# We use the Stefan-Boltzmann equation for radiation temperature loss given skin has a coefficient of emissivity near 1
+if(numberOfClothing==0):
+    surfaceArea = radius * 2 * math.pi * height
+
+    for i in range(totalStep):
+        diffTempFourthPower = math.pow(outerTemp, 4) - math.pow(bodyTemp, 4)
+        # diffTemp should be negative if outer temp is less than body temp
+
+        QDot = surfaceArea * diffTempFourthPower * STEFANBOLTZMANNCONSTANT
+        bodyTemp += stepSize * QDot / weight / WATERHEATCAPACITY
+        time.append((i+1)*stepSize)
+        bodyTempData.append(bodyTemp)
+
+    #graph(time, bodyTempData)
+
+
+
+else:
+    clothingTemp = outerTemp # We also wish to store clothingTemp data
+    clothingTempData = [outerTemp]
+    # I will be implementing simultaneous updates to numerically calculate integrals
+
+    surfaceArea = distanceOfClothing(numberOfClothing-1) * 2 * math.pi * height
+
+    for i in range(totalStep):
+        diffTempFourthPower = math.pow(outerTemp, 4) - math.pow(bodyTemp, 4)
+        # diffTemp should be negative if outer temp is less than body temp
+
+        QDot = surfaceArea * diffTempFourthPower * STEFANBOLTZMANNCONSTANT
+        bodyTemp += stepSize * QDot / weight / WATERHEATCAPACITY
+        time.append((i + 1) * stepSize)
+        bodyTempData.append(bodyTemp)
+
 
 
